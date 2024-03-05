9979ec6cb8d28f9911e8939bcbe558977976c7f7
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-03-05 16:49:24 +0200

    Update 1_ceturksnis_t207.R

--- 1_ceturksnis_t207.R
+++ 1_ceturksnis_t207.R
@@ -1,28 +1,34 @@
-#--------------- T207: N2z
+    #--------------- Tabula 207: N 2-zīmēs
 
-#1 Ielādē ADV ceturksni Q - šis solis sakrīt visiem t
+#1 Ielādē ADV ceturksni Q - šis solis sakrīt ar t911
 source(paste0("..//ADV_inputDF//Faila_ielade.R"))
 
-#2 No ADV datnes izveido N_zīmju aili ar nosaukumu B2z
+#2 No ADV datnes izveido N zīmju aili ar nosaukumu B2z (kā Burts un 2 zīmes)
 x$N2z[nchar(x$N2z) == 1] <- paste0("0", as.character(x$N2z[nchar(x$N2z) == 1]))
 x$B2z <- paste0(x$burts, x$N2z)
 
-## TE IET CASE SELECT
-# UZRAKSTI PAREIZI, BET DOMA IR TĀDA, JA KOPS 23 <- TAD ŠAJĀ VIETĀ IZMANTO 23 SCRIPTU, JA 01, TAD 01 utt
+#3 Izejot no tabulas numura, definē datu rindu.
 
-if(kopNr == "01" || kopNr == "23"|| kopNr == "24") {
-  X_n2z <-kops_noKSVT(x)
+rinda <- switch (tabula,
+  "207" = "X1441",
+  "208" = "X1442",
+  "911" = "X401"
+)
 
-  } else if (kopNr == "50" || kopNr == "85") {
-  source(paste0(base_path, "r-kods//kops50_85.R"))
-}
+if(kopsNr == "01" || kopsNr == "23"|| kopsNr == "24") {
+  X_n2z <- kopsavilkumi_noSVTK(x)
+  } else if (kopsNr == "50" || kopsNr == "85") {
+    X_n2z <- kopsavilkumi_noISK(x)
+    } else {
+      stop("Neatpazīts kopsavilkuma numurs.")
+      }
 
-# Sarēķina x1141 vērtības burtos
+# Sarēķinām vērtības burtos
 X_NBurti <- N_burti(X_n2z)
-X_TOTAL <- N_TOTAL(X_NBurti) #X_n2z - tabula X ar N 2. zīmēs. Apstrādātā izejas tabula x pārtop par tX, kur apstrāde notikusi uz N 2-ās zīmēs
+X_TOTAL <- N_TOTAL(X_NBurti) #X_n2z - tabula X ar N 2. zīmēs. Apstrādātā izejas tabula x pārtop par tabulu X, kur apstrāde notikusi uz N 2-ās zīmēs
 X_nBE <- N_BE(X_NBurti)      #X_nBE - tabula X, kas nes N sarēķinātos BE
 
-# Ceturksnis t207, kas tālāk pēc vajadzības piemērojams attiecīgiem komplektiem
+# Ceturksnis tabulai, kas tālāk pēc vajadzības piemērojams attiecīgiem komplektiem
 X <- rbind(X_TOTAL, X_nBE, X_NBurti, X_n2z)
 rm(X_TOTAL, X_nBE, X_NBurti, X_n2z)
 
@@ -36,7 +42,7 @@ N_komplekts <- "full"
 quaterlyPrintout(X, N_komplekts)
 rm(N_komplekts)
 
-#Saglabā kops t207 burtos, kas sevī apvieno TOTAL un N burtos.
+#Saglabā kopsavilkuma tabulu 207 burtos, kas sevī apvieno TOTAL un N burtos.
 N_komplekts <- "burti"
 quaterlyPrintout(X, N_komplekts)
-rm(N_komplekts)
+rm(N_komplekts, x, X)
